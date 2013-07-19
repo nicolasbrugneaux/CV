8cb8eed5d77ab4c99a7e293f9a4b1e2fc5043728
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2013-07-19 14:38:45 +0200

    typo

--- pages/blog.php
+++ pages/blog.php
@@ -1,7 +1,7 @@
 <div class="blog">
 	<div class="hero-unit">
 		<h1>Blog</h1>
-		<p>This is where I'll share my thougts, my discoveries or anything that I wanted to share with you.</p>
+		<p>This is where I'll share my thoughts, my discoveries or anything that I wanted to share with you.</p>
 	</div>
 		<div class="container">
 			<div class="progress">
